fa59709bc9b9a24d0b3cfa498f8aead105d6c13f
makisekurisu <kurikame1017@gmail.com>
2024-07-20 22:58:09 +0900

    [Fix] Dto 필드 수정

--- src/main/java/com/example/demo/domain/exam/dto/response/SearchExamResponse.java
+++ src/main/java/com/example/demo/domain/exam/dto/response/SearchExamResponse.java
@@ -7,12 +7,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SearchExamResponse {
     private String examName;
-    private String LocalDateTime;
+    private LocalDateTime createDate;
     private Difficulty difficulty;
     private Type type;
     private RevisionState revisionState;
