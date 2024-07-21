5df6b6e35098cac219adb604fca817afab82fb21
ohchansol <haxr369@gmail.com>
2024-07-21 20:47:57 +0900

    Feat : avro 2 Object mapper 구현 시작

---
+++ src/main/java/com/example/demo/domain/attempt/mapper/AttemptAnalysisDataMapper.java
@@ -0,0 +1,21 @@
+package com.example.demo.domain.attempt.mapper;
+
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class AttemptAnalysisDataMapper {
+//    public PaymentRequestAvroModel attemptAnalysisRequest(OrderCreatedEvent orderCreatedEvent) {
+//        Order order = orderCreatedEvent.getOrder();
+//        return PaymentRequestAvroModel.newBuilder()
+//                .setId(UUID.randomUUID().toString())
+//                .setSagaId("")
+//                .setCustomerId(order.getCustomerId().getValue().toString())
+//                .setOrderId(order.getId().getValue().toString())
+//                .setPrice(order.getPrice().getAmount())
+//                .setCreatedAt(orderCreatedEvent.getCreatedAt().toInstant())
+//                .setPaymentOrderStatus(PaymentOrderStatus.PENDING)
+//                .build();
+//    }
+}
