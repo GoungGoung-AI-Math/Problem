7763c407fa22c700ed70a2a870532482ca5cd645
ohchansol <haxr369@gmail.com>
2024-07-21 23:17:01 +0900

    Feat : 학생 풀이 분석 요청 맵퍼 생성

--- src/main/java/com/example/demo/kafka/mapper/AttemptAnalysisDataMapper.java
+++ src/main/java/com/example/demo/kafka/mapper/AttemptAnalysisDataMapper.java
@@ -1,14 +1,26 @@
 package com.example.demo.kafka.mapper;
 
 import com.example.demo.domain.attempt.dto.AttemptAnalysisRequest;
+import com.example.demo.my.kafka.infra.avrobuild.AnalysisType;
 import com.example.demo.my.kafka.infra.avrobuild.AttemptAnalysisRequestAvroModel;
+import com.example.demo.my.kafka.infra.avrobuild.MessageType;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Component
 public class AttemptAnalysisDataMapper {
-    public AttemptAnalysisRequestAvroModel attemptAnalysisRequest(AttemptAnalysisRequest attemptAnalysisRequest) {
-        return null;
+    public AttemptAnalysisRequestAvroModel attemptAnalysisRequestToAvroModel(AttemptAnalysisRequest attemptAnalysisRequest){
+        return AttemptAnalysisRequestAvroModel.newBuilder()
+                .setAttemptId(attemptAnalysisRequest.getAttemptId())
+                .setAnalysisType(AnalysisType.valueOf(
+                        attemptAnalysisRequest.getAnalysisType().name()
+                ))
+                .setMessageType(MessageType.valueOf(
+                        attemptAnalysisRequest.getContent()
+                ))
+                .build();
     }
 }
