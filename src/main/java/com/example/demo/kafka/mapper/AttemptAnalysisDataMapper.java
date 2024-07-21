5a8a52652a57295a1b385b1104fb886a37fb30f1
ohchansol <haxr369@gmail.com>
2024-07-21 21:32:15 +0900

    Setting subtree 의존성 얻기위한 설정

---
+++ src/main/java/com/example/demo/kafka/mapper/AttemptAnalysisDataMapper.java
@@ -0,0 +1,13 @@
+package com.example.demo.kafka.mapper;
+
+import com.example.demo.domain.attempt.dto.AttemptAnalysisRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class AttemptAnalysisDataMapper {
+//    public AttemptAnalysisRequestAvroModel attemptAnalysisRequest(AttemptAnalysisRequest attemptAnalysisRequest) {
+//        return null;
+//    }
+}
