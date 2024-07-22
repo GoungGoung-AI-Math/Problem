850a58a8ee316f7bc50e719419ab339ddd79b48a
ohchansol <haxr369@gmail.com>
2024-07-22 15:25:46 +0900

    Feat : 분석 요청 퍼블리셔 구현

---
+++ src/main/java/com/example/demo/domain/attempt/kafka/publisher/AttemptAnalysisRequestPublisher.java
@@ -0,0 +1,41 @@
+package com.example.demo.domain.attempt.kafka.publisher;
+
+import com.example.demo.domain.attempt.kafka.event.AttemptAnalysisEvent;
+import com.example.demo.domain.attempt.kafka.mapper.AttemptAnalysisDataMapper;
+import com.example.demo.my.kafka.infra.avrobuild.AttemptAnalysisResponseAvroModel;
+import com.example.demo.my.kafka.infra.kafka.config.ProblemServiceKafkaConfigData;
+import com.example.demo.my.kafka.infra.kafka.producer.KafkaProducer;
+import com.example.demo.my.kafka.infra.kafka.publisher.kafka.DomainEventPublisher;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AttemptAnalysisRequestPublisher implements DomainEventPublisher<AttemptAnalysisEvent> {
+
+    private final AttemptAnalysisDataMapper attemptAnalysisDataMapper;
+    private final KafkaProducer<String, AttemptAnalysisResponseAvroModel> kafkaProducer;
+    private final ProblemServiceKafkaConfigData problemServiceKafkaConfigData;
+    @Override
+    public void publish(AttemptAnalysisEvent domainEvent) {
+        Long attemptId = domainEvent.getAttemptAnalysisDto().getAttemptId();
+        log.info("Received AttemptAnalysisEvent for attempt id: {}", attemptId);
+        try {
+            AttemptAnalysisResponseAvroModel attemptAnalysisResponseAvroModel =
+                    attemptAnalysisDataMapper
+                            .attemptAnalysisResponseToAvroModel(domainEvent.getAttemptAnalysisDto());
+
+            kafkaProducer.send(problemServiceKafkaConfigData.getAttemptAnalysisResponseTopicName(),
+                    String.valueOf(attemptId),
+                    attemptAnalysisResponseAvroModel);
+
+            log.info("AttemptAnalysisResponseAvroModel sent to kafka at: {}", System.nanoTime());
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("Error while sending AttemptAnalysisResponseAvroModel message" +
+                    " to kafka with order id: {}, error: {}", attemptId, e.getMessage());
+        }
+    }
+}
