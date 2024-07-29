32ae36bed88dacc1dce202f219a996e344a925d9
ohchansol <haxr369@gmail.com>
2024-07-30 02:02:54 +0900

    subtree 사용 위치 수정

--- src/main/java/Math/AI/my/kafka/infra/kafka/producer/exception/KafkaProducerException.java
+++
@@ -1,8 +0,0 @@
-package Math.AI.my.kafka.infra.kafka.producer.exception;
-
-public class KafkaProducerException extends RuntimeException {
-
-    public KafkaProducerException(String message) {
-        super(message);
-    }
-}
