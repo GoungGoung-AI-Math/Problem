16d00ff375b10b1e1d1ca49dc076f75f5ccd512c
makisekurisu <kurikame1017@gmail.com>
2024-08-04 19:21:02 +0900

    [Fix] QuestionList API 반환 형식 text, imgurl 나눔

--- src/main/java/com/example/demo/domain/question/mvc/repository/QuestionRepositoryCustomImpl.java
+++ src/main/java/com/example/demo/domain/question/mvc/repository/QuestionRepositoryCustomImpl.java
@@ -1,7 +1,10 @@
 package com.example.demo.domain.question.mvc.repository;
 
+import com.example.demo.domain.question.mvc.dto.QuestionContent;
 import com.example.demo.domain.question.mvc.dto.QuestionResponse;
 import com.example.demo.domain.question.mvc.entity.QQuestion;
+import com.example.demo.domain.question.mvc.entity.QQuestionContentEntity;
+import com.example.demo.domain.question.mvc.entity.QuestionContentEntity;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,27 +23,30 @@ public class QuestionRepositoryCustomImpl implements QuestionRepositoryCustom {
     @Override
     public List<QuestionResponse> findQuestionListByProblemId(Long problemId) {
         QQuestion question = QQuestion.question;
+        QQuestionContentEntity contentEntity = QQuestionContentEntity.questionContentEntity;
 
 
         List<QuestionResponse> results = jpaQueryFactory
                 .select(Projections.constructor(QuestionResponse.class,
                         question.problem.id,
                         question.userId,
-                        question.title
+                        question.title,
+                        question.content
                 ))
                 .from(question)
                 .where(question.problem.id.eq(problemId))
                 .fetch();
 
         for (QuestionResponse response : results) {
-            List<String> contents = jpaQueryFactory
-                    .select(question.content)
-                    .from(question)
-                    .where(question.problem.id.eq(problemId))
+            List<String> imgUrls = jpaQueryFactory
+                    .select(contentEntity.imgUrl)
+                    .from(contentEntity)
+                    .where(contentEntity.question.id.eq(response.getUserId())) // Use correct foreign key
                     .fetch();
-            response.setContents(contents);
+
+            response.setImgUrls(imgUrls);
         }
+
         return results;
     }
-
 }
